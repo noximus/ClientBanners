4d6a007e2782d1c99ce8fcbae852217a5ad9a52b
HENRY RICHARDSON <Noximus@Noximus.home>
2015-08-21 22:01:40 -0400

    cleaned up folders

---
+++ DestinyTakenKing970x66/manifest.js
@@ -0,0 +1,9 @@
+FT.manifest({
+	"filename":"main.html",
+	"width":300,
+	"height":250,
+	"clickTagCount":1,
+	"instantAds":[
+		{"name":"textVar", "type":"text", "default":"XXXXXXXXXXXXXXXXXXXXXX"}
+	]
+});
