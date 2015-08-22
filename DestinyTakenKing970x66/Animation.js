494d8e15e516688ffa188b0e6467cd5f848fa548
HENRY RICHARDSON <Noximus@Noximus.home>
2015-08-22 01:27:55 -0400

    970x66 under 15 seconds

--- DestinyTakenKing970x66/Animation.js
+++ DestinyTakenKing970x66/Animation.js
@@ -28,27 +28,10 @@ home_animation = (function() {
 			}
 		}
 	}
-	
-
-		
-
-	function Reset()
-	{
-		//TweenLite.killDelayedCallsTo([bg, frame1, frame2, frame3, frame4]);
-		/*TweenMax.killAll(false, false, true);
-		TweenMax.killAllTweens();*/
-		
-	}
-	
 	function init()
 	{
-	
-		// TweenLite.to(end_frame_heart, 0, {scaleX:0, scaleY:0});
-		frame1();
-		
-	
+		frame1();	
 	}
-	
 		// vars	
 		var enterFor = document.getElementById("enterFor");
 		var prize = document.getElementById("prize");
@@ -68,10 +51,9 @@ home_animation = (function() {
 
 	function frame1()
 	{
-		
 		TweenLite.to(enterFor, .8, {delay: .3, opacity: 1, ease: Linear.eastOut});
 		TweenLite.to(prize, .4, {delay:1.5, opacity: 1, ease: Linear.eastOut});
-		TweenLite.delayedCall(4.2, frame2);
+		TweenLite.delayedCall(4, frame2);
 	}
 	
 	function frame2()
@@ -79,8 +61,7 @@ home_animation = (function() {
 
 		TweenLite.to(enter, 0.4, {opacity: 0, display: 'none', ease: Linear.easeOut});
 		TweenLite.to(tweet, 0.3, {delay:.5, display: 'block', opacity: 1, ease: Linear.easeIn});
-		TweenLite.delayedCall(5, frame3);
-		
+		TweenLite.delayedCall(4, frame3);
 	}
 	
 	function frame3()
@@ -89,7 +70,7 @@ home_animation = (function() {
 		TweenLite.to(heroFrame, 0.3, {delay:.5, display: 'block', ease: Linear.easeIn});
 		TweenLite.to(herosbg, 0.6, {delay:1, display: 'block', opacity:1, ease: Linear.easeIn});
 		TweenLite.to(hashtag, 1, {delay:1.5, display: 'block', opacity:1, ease: Linear.easeIn});
-		TweenLite.delayedCall(6, frame4);
+		TweenLite.delayedCall(4.5, frame4);
 	}
 	
 	function frame4()
@@ -105,26 +86,7 @@ home_animation = (function() {
 		TweenLite.to(lrnBtn, 0.5, {delay: 3, opacity: 1, display:'block', ease: Linear.easeIn});
 	}
 		
-	
-	
 	init();
 });
 
-
-
-
-
-// If true, start function. If false, listen for INIT.
-// window.onload = function() {
-//   if (Enabler.isInitialized()) {
-//       enablerInitHandler();
-//   } else {
-//       Enabler.addEventListener(studio.events.StudioEvent.INIT,
-// enablerInitHandler);
-//   }
-// }
-
-// function enablerInitHandler() {
-//   home_animation();
-// }
 home_animation();
