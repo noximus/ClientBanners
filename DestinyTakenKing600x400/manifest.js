bae81feac666678bf8b518af24d866cdd312652b
Henry Richardson <hrichardson83@gmail.com>
2015-09-01 13:12:35 -0400

    added folders as place holders and updated images for 970x66

---
+++ DestinyTakenKing600x400/manifest.js
@@ -0,0 +1,9 @@
+FT.manifest({
+	"filename":"index.html",
+	"width":300,
+	"height":600,
+	"clickTagCount":1,
+	"instantAds":[
+		{"name":"textVar", "type":"text", "default":"Real-life guardian armor Choose Hunter, Titan, or Warlock"}
+	]
+});
