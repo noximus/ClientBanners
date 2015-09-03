eefe105787cd61e40c86b566645df2796502dcc9
Henry Richardson <hrichardson83@gmail.com>
2015-09-03 11:47:22 -0400

    300x600 frame 1 and 2 fixes

--- DestinyTakenKing300x600/manifest.js
+++ DestinyTakenKing300x600/manifest.js
@@ -2,8 +2,5 @@ FT.manifest({
 	"filename":"index.html",
 	"width":300,
 	"height":600,
-	"clickTagCount":1,
-	"instantAds":[
-		{"name":"textVar", "type":"text", "default":"Real-life guardian armor Choose Hunter, Titan, or Warlock"}
-	]
+	"clickTagCount":1
 });
