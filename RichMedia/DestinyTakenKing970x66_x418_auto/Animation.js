45848a190dcbfe524187795a3362016c11d6e3e4
Henry Richardson <hrichardson83@gmail.com>
2015-09-03 18:12:34 -0400

    fixed auto for 970x418

--- RichMedia/DestinyTakenKing970x66_x418_auto/Animation.js
+++ RichMedia/DestinyTakenKing970x66_x418_auto/Animation.js
@@ -32,23 +32,7 @@ home_animation = (function() {
 	{
 		frame1Ex();	
 	}
-		// vars	
-		var container = document.getElementById("container");
-		var enterFor = document.getElementById("enterFor");
-		var prize = document.getElementById("prize");
-		var tweet = document.getElementById("tweet");
-		var enter = document.getElementById("enter");
-		var heroFrame = document.getElementById("heroFrame");		
-		var heros = document.getElementById("heros");	
-		var hashtag = document.getElementById("hashtag");	
-		var greatness = document.getElementById("greatness");
-		var finalFrame = document.getElementById("finalFrame");
-		var lrnBtn = document.getElementById("lrnBtn");
-		var wave = document.getElementById("wave");
-		var logo = document.getElementById("logo");
-		var learnMore = document.getElementById("learnMore");
-		var ps4logo = document.getElementById("ps4logo");
-		var bundlesweeps = document.getElementById("bundlesweeps");
+		// vars
 
 		var containerEx = document.getElementById("containerEx");
 		var bloodEx = document.getElementById("bloodEx");
@@ -67,6 +51,106 @@ home_animation = (function() {
 		var learnMoreEx = document.getElementById("learnMoreEx");
 		var ps4logoEx = document.getElementById("ps4logoEx");
 		var bundlesweepsEx = document.getElementById("bundlesweepsEx");
+
+	
+	function frame1Ex()
+	{
+		// vid.mute();
+		// vid.play();
+		TweenLite.to(container, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
+		TweenLite.to(containerEx, .5, {opacity:1, display: 'block', ease:Linear.easeInOut});
+		TweenLite.to(enterForEx, 1, {delay: .3, opacity: 1, ease: Linear.eastOut});
+		TweenLite.to(prizeEx, 1, {delay:.7, opacity: 1, ease: Linear.eastOut});
+		TweenLite.delayedCall(3, frame2Ex);
+	}
+	
+	function frame2Ex()
+	{
+		TweenLite.to(prizeEx, 0.4, {opacity: 0, ease: Linear.eastOut});
+		TweenLite.to(enterEx, 0.4, {opacity: 0, display: 'none', ease: Linear.easeOut});
+		TweenLite.to(tweetEx, 0.3, {delay:.5, display: 'block', opacity: 1, ease: Linear.easeIn});
+		TweenLite.delayedCall(3, frame3Ex);
+	}
+	
+	function frame3Ex()
+	{
+		TweenLite.to(tweetEx, 0.5, {opacity: 0, display: 'none', ease: Linear.easeOut});
+		TweenLite.to(heroFrameEx, 0.3, {delay:.5, display: 'block', ease: Linear.easeIn});
+		TweenLite.to(herosbgEx, 0.6, {delay:1, display: 'block', opacity:1, ease: Linear.easeIn});
+		TweenLite.to(hashtagEx, 1, {delay:1.5, display: 'block', opacity:1, ease: Linear.easeIn});
+		TweenLite.delayedCall(3.5, frame4Ex);
+	}
+	
+	function frame4Ex()
+	{
+		TweenLite.to(heroFrameEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
+		TweenLite.to(bloodEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
+		TweenLite.to(logoEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
+		TweenLite.to(learnMoreEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
+		TweenLite.to(finalFrameEx, .5, {delay: 1, opacity: 1, display: 'block', ease: Linear.easeIn});
+		// TweenLite.to(ps4logo, .5, {delay: 1.5, opacity:1, display: 'block', ease:Linear.easeIn});
+		TweenLite.to(greatnessEx, 0.5, {delay: 2, opacity: 1, display:'block', ease: Linear.easeIn});
+		TweenLite.to(bundlesweepsEx, .5, {delay:2.5, opacity:1, display: 'block', ease:Linear.easeIn});
+		TweenLite.to(lrnBtnEx2, 0.5, {delay: 3, opacity: 1, display:'block', ease: Linear.easeIn});
+	}
+		
+	init();
+});
+
+home_animation();
+
+home_animation2 = (function() {
+	
+	var boundEvents = {};
+
+	function bind (elem, eventName, callback) {
+		if (elem.addEventListener) {
+			elem.addEventListener(eventName, callback, false);
+		}
+
+		else if (elem.attachEvent) {
+			var eID = elem.attachEvent('on'+ eventName, callback);
+			boundEvents[eID] = { name: eventName, callback: callback };
+		}
+	}
+
+	function unbind (elem, eventName, callback) {
+		if (elem.removeEventListener) {
+			elem.removeEventListener(eventName, callback, false);
+		}
+
+		else if (elem.detachEvent) {
+			for (var eID in boundEvents) {
+				if ((boundEvents[eID].name === eventName) &&
+						(boundEvents[eID].callback === callback)) {
+					elem.detachEvent(eID);
+					delete boundEvents[eID];
+				}
+			}
+		}
+	}
+	function init()
+	{
+		frame1();	
+	}
+		// vars	
+		var container = document.getElementById("container");
+		var enterFor = document.getElementById("enterFor");
+		var prize = document.getElementById("prize");
+		var tweet = document.getElementById("tweet");
+		var enter = document.getElementById("enter");
+		var heroFrame = document.getElementById("heroFrame");		
+		var heros = document.getElementById("heros");	
+		var hashtag = document.getElementById("hashtag");	
+		var greatness = document.getElementById("greatness");
+		var finalFrame = document.getElementById("finalFrame");
+		var lrnBtn = document.getElementById("lrnBtn");
+		var wave = document.getElementById("wave");
+		var logo = document.getElementById("logo");
+		var learnMore = document.getElementById("learnMore");
+		var ps4logo = document.getElementById("ps4logo");
+		var bundlesweeps = document.getElementById("bundlesweeps");
+
 
 	function frame1()
 	{
@@ -103,50 +187,8 @@ home_animation = (function() {
 		TweenLite.to(greatness, 0.5, {delay: 2, opacity: 1, display:'block', ease: Linear.easeIn});
 		TweenLite.to(bundlesweeps, .5, {delay:2.5, opacity:1, display: 'block', ease:Linear.easeIn});
 		TweenLite.to(lrnBtn, 0.5, {delay: 3, opacity: 1, display:'block', ease: Linear.easeIn});
-		TweenLite.delayedCall(4.5, frame1Ex);
-	}
-	function frame1Ex()
-	{
-		// vid.mute();
-		// vid.play();
-		TweenLite.to(container, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
-		TweenLite.to(containerEx, .5, {opacity:1, display: 'block', ease:Linear.easeInOut});
-		TweenLite.to(enterForEx, 1, {delay: .3, opacity: 1, ease: Linear.eastOut});
-		TweenLite.to(prizeEx, 1, {delay:.7, opacity: 1, ease: Linear.eastOut});
-		TweenLite.delayedCall(4, frame2Ex);
 	}
 	
-	function frame2Ex()
-	{
-		TweenLite.to(prizeEx, 0.4, {opacity: 0, ease: Linear.eastOut});
-		TweenLite.to(enterEx, 0.4, {opacity: 0, display: 'none', ease: Linear.easeOut});
-		TweenLite.to(tweetEx, 0.3, {delay:.5, display: 'block', opacity: 1, ease: Linear.easeIn});
-		TweenLite.delayedCall(4, frame3Ex);
-	}
-	
-	function frame3Ex()
-	{
-		TweenLite.to(tweetEx, 0.5, {opacity: 0, display: 'none', ease: Linear.easeOut});
-		TweenLite.to(heroFrameEx, 0.3, {delay:.5, display: 'block', ease: Linear.easeIn});
-		TweenLite.to(herosbgEx, 0.6, {delay:1, display: 'block', opacity:1, ease: Linear.easeIn});
-		TweenLite.to(hashtagEx, 1, {delay:1.5, display: 'block', opacity:1, ease: Linear.easeIn});
-		TweenLite.delayedCall(4.5, frame4Ex);
-	}
-	
-	function frame4Ex()
-	{
-		TweenLite.to(heroFrameEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
-		TweenLite.to(bloodEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
-		TweenLite.to(logoEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
-		// TweenLite.to(learnMoreEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
-		TweenLite.to(finalFrameEx, .5, {delay: 1, opacity: 1, display: 'block', ease: Linear.easeIn});
-		// TweenLite.to(ps4logo, .5, {delay: 1.5, opacity:1, display: 'block', ease:Linear.easeIn});
-		TweenLite.to(greatnessEx, 0.5, {delay: 2, opacity: 1, display:'block', ease: Linear.easeIn});
-		TweenLite.to(bundlesweepsEx, .5, {delay:2.5, opacity:1, display: 'block', ease:Linear.easeIn});
-		// TweenLite.to(lrnBtnEx, 0.5, {delay: 3, opacity: 1, display:'block', ease: Linear.easeIn});
-	}
 		
 	init();
 });
-
-home_animation();
