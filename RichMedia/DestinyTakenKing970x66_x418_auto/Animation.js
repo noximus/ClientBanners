ee7c784f69b1c4c7cbdc68f16869edcef19e2100
Henry Richardson <hrichardson83@gmail.com>
2015-09-08 11:47:47 -0400

    970x66 418 auto expand works like batman banners

--- RichMedia/DestinyTakenKing970x66_x418_auto/Animation.js
+++ RichMedia/DestinyTakenKing970x66_x418_auto/Animation.js
@@ -39,7 +39,7 @@ var home_animation = {
   frame1: function () {
     TweenLite.to(enterFor, .8, {delay: .3, opacity: 1, ease: Linear.eastOut});
     TweenLite.to(prize, .8, {delay:1.3, opacity: 1, ease: Linear.eastOut});
-    TweenLite.delayedCall(4, home_animation.frame4);
+    TweenLite.delayedCall(4, home_animation.frame2);
   },
   frame2: function () {
     TweenLite.to(prize, 0.4, {opacity: 0, ease: Linear.eastOut});
@@ -58,19 +58,21 @@ var home_animation = {
     TweenLite.to(heroFrame, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
     TweenLite.to(logo, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
     TweenLite.to(learnMore, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
-    TweenLite.to(finalFrame, .5, {delay: 1, opacity: 1, display: 'block', ease: Linear.easeIn});
-    TweenLite.to(greatness, 0.5, {delay: 2, opacity: 1, display:'block', ease: Linear.easeIn});
-    TweenLite.to(bundlesweeps, .5, {delay:2.5, opacity:1, display: 'block', ease:Linear.easeIn});
-    TweenLite.to(lrnBtn, 0.5, {delay: 3, opacity: 1, display:'block', ease: Linear.easeIn});
-  },
-  frame4Out: function () {
-    TweenLite.to(heroFrame, 0, {opacity:0, display: 'none', ease:Linear.easeInOut});
-    TweenLite.to(logo, 0, {opacity:0, display: 'none', ease:Linear.easeInOut});
-    TweenLite.to(learnMore, 0, {opacity:0, display: 'none', ease:Linear.easeInOut});
-    TweenLite.to(finalFrame, 0, {delay: 1, opacity: 1, display: 'block', ease: Linear.easeIn});
+    TweenLite.to(finalFrame, .5, {delay: 0, opacity: 1, display: 'block', ease: Linear.easeIn});
     TweenLite.to(greatness, 0.5, {delay: 1, opacity: 1, display:'block', ease: Linear.easeIn});
-    TweenLite.to(bundlesweeps, 1, {delay:1.5, opacity:1, display: 'block', ease:Linear.easeIn});
-    TweenLite.to(lrnBtn, 1.5, {delay: 3, opacity: 1, display:'block', ease: Linear.easeIn});
+    TweenLite.to(bundlesweeps, .5, {delay:1.5, opacity:1, display: 'block', ease:Linear.easeIn});
+    TweenLite.to(lrnBtn, 0.5, {delay: 2, opacity: 1, display:'block', ease: Linear.easeIn});
+  },
+  frame1ExIntro: function () {
+    TweenLite.to(finalFrameEx, 0, {delay: 0, opacity: 0, display: 'block', ease: Linear.easeIn});
+    TweenLite.to(greatnessEx, 0, {delay: 0, opacity: 0, display:'block', ease: Linear.easeIn});
+    TweenLite.to(bundlesweepsEx, 0, {delay:0, opacity:0, display: 'block', ease:Linear.easeIn});
+    TweenLite.to(lrnBtnEx2, 0, {delay: 0, opacity: 0, display:'block', ease: Linear.easeIn});
+    TweenLite.to(enterEx, 0, {delay: 0, opacity: 1, display: 'block', ease: Linear.easeOut});
+    TweenLite.to(bloodEx, 0, {opacity:1, display: 'block', ease:Linear.easeInOut});
+    TweenLite.to(logoEx, 0, {opacity:1, display: 'block', ease:Linear.easeInOut});
+    TweenLite.to(learnMoreEx, 0, {opacity:1, display: 'block', ease:Linear.easeInOut});
+    TweenLite.delayedCall(0, home_animation.frame1Ex);
   },
   frame1Ex: function () {
     TweenLite.to(container, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
@@ -87,7 +89,7 @@ var home_animation = {
   },
   frame3Ex: function () {
     TweenLite.to(tweetEx, 0.5, {opacity: 0, display: 'none', ease: Linear.easeOut});
-    TweenLite.to(heroFrameEx, 0.3, {delay:.5, display: 'block', ease: Linear.easeIn});
+    TweenLite.to(heroFrameEx, 0.3, {delay:.5, display: 'block',opacity:1, ease: Linear.easeIn});
     TweenLite.to(herosbgEx, 0.6, {delay:1, display: 'block', opacity:1, ease: Linear.easeIn});
     TweenLite.to(hashtagEx, 1, {delay:1.5, display: 'block', opacity:1, ease: Linear.easeIn});
     TweenLite.delayedCall(3.5, home_animation.frame4Ex);
@@ -97,10 +99,10 @@ var home_animation = {
     TweenLite.to(bloodEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
     TweenLite.to(logoEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
     TweenLite.to(learnMoreEx, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
-    TweenLite.to(finalFrameEx, .5, {delay: 1, opacity: 1, display: 'block', ease: Linear.easeIn});
-    TweenLite.to(greatnessEx, 0.5, {delay: 2, opacity: 1, display:'block', ease: Linear.easeIn});
-    TweenLite.to(bundlesweepsEx, .5, {delay:2.5, opacity:1, display: 'block', ease:Linear.easeIn});
-    TweenLite.to(lrnBtnEx2, 0.5, {delay: 3, opacity: 1, display:'block', ease: Linear.easeIn});
+    TweenLite.to(finalFrameEx, .5, {delay: 0, opacity: 1, display: 'block', ease: Linear.easeIn});
+    TweenLite.to(greatnessEx, 0.5, {delay: 1, opacity: 1, display:'block', ease: Linear.easeIn});
+    TweenLite.to(bundlesweepsEx, .5, {delay:1.5, opacity:1, display: 'block', ease:Linear.easeIn});
+    TweenLite.to(lrnBtnEx2, 0.5, {delay: 2, opacity: 1, display:'block', ease: Linear.easeIn});
   }
 };
 
