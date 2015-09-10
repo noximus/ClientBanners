6fbc855ea94749b470ce5bd44aaa0ec18cfff168
Henry Richardson <hrichardson83@gmail.com>
2015-09-10 14:28:11 -0400

    970x250 > 970x418 Click To  - no purchase necessary - on close, ESRB logo appears on 4th frame and disappears after awhile, when it should not be on 4th frame at al

--- RichMedia/Destiny970x250_x418_clickTo/Animation.js
+++ RichMedia/Destiny970x250_x418_clickTo/Animation.js
@@ -37,10 +37,18 @@ var home_animation = {
   learnMoreEx: document.getElementById("learnMoreEx"),
   ps4logoEx: document.getElementById("ps4logoEx"),
   bundlesweepsEx: document.getElementById("bundlesweepsEx"),
-  noPurch: document.getElementById("noPurch"),
+  noPurch: document.getElementById("noPurch"), 
+  noPurchEx: document.getElementById("noPurchEx"), 
   frame1: function () {
-    TweenLite.to(enterFor, .8, {delay: .3, opacity: 1, ease: Linear.eastOut});
-    TweenLite.to(prize, .8, {delay:1.3, opacity: 1, ease: Linear.eastOut});
+    TweenLite.to(finalFrame, 0, {delay: 0, opacity: 0, display: 'none', ease: Linear.easeIn});
+    TweenLite.to(greatness, 0, {delay: 0, opacity: 0, display:'none', ease: Linear.easeIn});
+    TweenLite.to(bundlesweeps, 0, {delay:0, opacity:0, display: 'none', ease:Linear.easeIn});
+    TweenLite.to(lrnBtn, 0, {delay: 0, opacity: 0, display:'none', ease: Linear.easeIn});
+    TweenLite.to(logo, 0, {opacity:1, display: 'block', ease:Linear.easeInOut});
+    TweenLite.to(learnMore, 0, {opacity:1, display: 'block', ease:Linear.easeInOut});
+    TweenLite.to(enter, .8, {delay: .3, opacity: 1,display: 'block', ease: Linear.eastOut});
+    TweenLite.to(enterFor, .8, {delay: .3, opacity: 1,display: 'block', ease: Linear.eastOut});
+    TweenLite.to(prize, .8, {delay:1.3, opacity: 1,display: 'block', ease: Linear.eastOut});
     TweenLite.to(blood, 0, {opacity:1, display: 'block', ease:Linear.easeInOut});
     TweenLite.delayedCall(4, home_animation.frame2);
   },
@@ -52,9 +60,10 @@ var home_animation = {
   },
   frame3: function () {
     TweenLite.to(tweet, 0.5, {opacity: 0, display: 'none', ease: Linear.easeOut});
-    TweenLite.to(heroFrame, 0.3, {delay:.5, display: 'block', ease: Linear.easeIn});
+    TweenLite.to(heroFrame, 0.3, {delay:.5, display: 'block', opacity:1, ease: Linear.easeIn});
     TweenLite.to(herosbg, 0.6, {delay:1, display: 'block', opacity:1, ease: Linear.easeIn});
     TweenLite.to(hashtag, 1, {delay:1.5, display: 'block', opacity:1, ease: Linear.easeIn});
+    TweenLite.to(noPurch, .6, {delay:2, display: 'block', opacity:1, ease: Linear.easeIn});
     TweenLite.delayedCall(4.5, home_animation.frame4);
   },
   frame4: function () {
@@ -81,8 +90,8 @@ var home_animation = {
   frame1Ex: function () {
     TweenLite.to(container, .5, {opacity:0, display: 'none', ease:Linear.easeInOut});
     TweenLite.to(containerEx, .5, {opacity:1, display: 'block', ease:Linear.easeInOut});
-    TweenLite.to(enterForEx, 1, {delay: .3, opacity: 1, ease: Linear.eastOut});
-    TweenLite.to(prizeEx, 1, {delay:.7, opacity: 1, ease: Linear.eastOut});
+    TweenLite.to(enterForEx, 1, {delay: .3, opacity: 1,display: 'block', ease: Linear.eastOut});
+    TweenLite.to(prizeEx, 1, {delay:.7, opacity: 1, display: 'block',ease: Linear.eastOut});
     TweenLite.delayedCall(3, home_animation.frame2Ex);
   },
   frame2Ex: function () {
@@ -96,7 +105,9 @@ var home_animation = {
     TweenLite.to(heroFrameEx, 0.3, {delay:.5, display: 'block',opacity:1, ease: Linear.easeIn});
     TweenLite.to(herosbgEx, 0.6, {delay:1, display: 'block', opacity:1, ease: Linear.easeIn});
     TweenLite.to(hashtagEx, 1, {delay:1.5, display: 'block', opacity:1, ease: Linear.easeIn});
-    TweenLite.to(noPurch, .6, {delay:2, display: 'block', opacity:1, ease: Linear.easeIn});
+    
+    TweenLite.to(noPurchEx, .6, {delay:2, display: 'block', opacity:1, ease: Linear.easeIn});
+
     TweenLite.delayedCall(4, home_animation.frame4Ex);
   },
   frame4Ex: function () {
